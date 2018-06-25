153ed41e592714f3d018507ef81feb27f92ea2bc
Dilven <mateusiakkrystian@gmail.com>
2018-06-25 23:58:54 +0200

    add likes and comment to photo

--- client/components/PhoteGrid.js
+++ client/components/PhoteGrid.js
@@ -2,11 +2,10 @@ import React from 'react';
 import Photo from './Photo'
 
 const PhotoGrid = ({ posts }) => {
-  console.log(posts)
   return (
     <div className="photo-grid">
     {posts.map((post, index) => {
-      return <Photo key={index} i={index} post={post}/>
+      return <Photo key={index} index={index} post={post}/>
     })}
     </div>
   )
--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
-const Photo = ({ post, key }) => {
-  console.log(post)
+const Photo = ({ post, index }) => {
   return (
-    <div key={key}>
     <figure className="grid-figure">
       <div className="grid-photo-wrap">
-        <Link>
+        <Link to={`/view/${post.code}`}>
           <img src={post.display_src} alt={post.caption} className="grid-photo"/>
         </Link>
+        <CSSTransitionGroup  transitionName="like"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+          <span key={post.likes} className="likes-heart">{post.likes}</span>
+        </CSSTransitionGroup>
       </div>
+    <figcaption>
+      <p>{post.caption}</p>
+      <div className="control-button">
+        <button className="likes">&hearts; {post.likes}</button>
+      </div>
+    </figcaption>
     </figure>
-    </div>
   )
 }
 
